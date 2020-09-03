68add73947edb7264f8386ca9a158823c5c30ec7
jawish-a@outlook.com <jawish-a@outlook.com>
2020-09-03 11:13:14 +0300

    completed the project

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,18 +1,44 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm, SignupForm, SigninForm
+from django.contrib.auth import login, logout, authenticate
 
 def signup(request):
-    
+    form = SignupForm
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(new_user.password)
+            new_user.save()
+
+            login(request, new_user)
+            return redirect('restaurant-list')
+            
+    context = {
+        "form":form,
+    }    
     return render(request, 'signup.html', context)
 
 def signin(request):
-    
-    return 
+    form = SigninForm
+    if request.method == "POST":
+        form = SigninForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            auth_user = authenticate(username=username, password=password)
+            if auth_user is not None:
+                login(request, auth_user)
+                return redirect('restaurant-list')
+    context = {
+        "form":form,
+    }    
+    return render(request, 'signin.html', context)
 
 def signout(request):
-    
-    return 
+    logout(request)
+    return redirect('signin') 
 
 def restaurant_list(request):
     context = {
